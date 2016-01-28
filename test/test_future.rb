3419a54344f1043a9a1dd4c2dda164bd7b7ade66
Lin Jen-Shin <godfat@godfat.org>
2016-01-28 20:53:09 +0800

    add more tests for future

--- test/test_future.rb
+++ test/test_future.rb
@@ -2,11 +2,21 @@
 require 'promise_pool/test'
 
 describe PromisePool::Future do
+  would 'return the value' do
+    Promise.new.defer{ 'value' }.future.should.eq 'value'
+  end
+
   would 'raise an exception' do
     expect.raise(RuntimeError) do
       Promise.new.defer do
         raise 'nnf'
       end.future.oops
+    end.message.should.eq 'nnf'
+  end
+
+  would 'raise an exception if it is returning an exception' do
+    expect.raise(RuntimeError) do
+      Promise.new.defer{ RuntimeError.new('nnf') }.future.oops
     end.message.should.eq 'nnf'
   end
 end
